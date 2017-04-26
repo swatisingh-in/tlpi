5b1340e151090aae3d6e9cd9243a75ae2fcfd3e8
inssingh025 <inssingh025@inssingh025-VirtualBox.(none)>
2017-04-26 12:35:55 +0530

    Chapter - 29 (Threads Introduction) exercises

--- chapter-29/thread.c
+++ chapter-29/thread.c
@@ -1,1 +1,48 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <errno.h>
+#include <string.h>	
 
+pthread_t tid;
+pthread_t thread2;
+
+void * thread_two()
+{
+	printf("thread_two says hello\n");
+	return (void *)NULL;
+}
+
+void * thread_one()
+{
+	printf("thread_one execution start here\n");
+
+	if (pthread_equal(tid, pthread_self()))
+		printf("Both the threads are equal\n");
+
+	int retval = pthread_create(&thread2, NULL, &thread_two, NULL);
+
+	if(retval != 0) {
+		printf("pthread_create of thread2 failed: %s\n", strerror(errno));
+		return (void *)-1;
+	}
+
+	pthread_join(thread2, NULL);
+	printf("thread_one execution finished here\n");
+	return (void *) NULL;
+}
+
+int main(int argc, char const *argv[])
+{
+	printf("Main execution starts from here\n");
+
+	int ret = pthread_create(&tid, NULL, &thread_one, NULL);
+
+	if(ret != 0){
+		printf("pthread_create failed: %s\n", strerror(errno));
+		return -1;
+	}
+
+	pthread_join(tid, NULL);
+	printf("Main execution finished here\n");
+	return 0;
+}
--- chapter-29/thread_tlpi.c
+++ chapter-29/thread_tlpi.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
 
+void *thread_one(void *arg)
+{
+	char *str = (char *) arg;
+	printf("thread_one: %s\n", str);
+	return (void *)strlen(str);
+}
+
+int main(int argc, char const *argv[])
+{
+	pthread_t tid;
+	void *retval;
+	int ret;
+
+	ret = pthread_create(&tid, NULL, thread_one, "Hello World");
+	if(ret != 0){
+		printf("pthread_create failed\n");
+		exit(-1);
+	}
+
+	printf("Message from Main\n");
+
+	ret = pthread_join(tid, &retval);
+	if (ret != 0) {
+		printf("pthread_join failed\n");
+		exit(-1);
+	}
+
+	printf("Thread returned: %ld\n", (long int)retval);
+	printf("Bye Bye from Main\n");
+	exit(0);
+}
